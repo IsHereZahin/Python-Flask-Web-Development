d68599af496fc4b0480bfcf8b547b633080c073e
isherezahin <abuzahinmohammadnowsin@gmail.com>
2024-10-12 15:11:26 +0600

    Update app code to pass data to index.html template

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Route for the home page
+datas = {
+    "name": "Zahin",
+    "numbers":[1,2,3,4,5,6,7,8,9]
+}
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', data=datas)
 
 # Route for the About page
 @app.route("/about")
