ff5921ed7f186a1e2a202d59e2744f77685b804c
isherezahin <abuzahinmohammadnowsin@gmail.com>
2024-10-12 14:32:10 +0600

    Routes: Added home and about routes to Flask app

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-print("Hello World")
+# Route for the home page
+@app.route("/")
+def home():
+    return "<h1 style='color:#0D1117; text-align:center;'>Welcome to the Flask homepage</h1>"
+
+# Route for the About page
+@app.route("/about")
+def about():
+    return "<h1 style='color:#0D1117;'>This is the Flask 'About' page</h1>"
 
 if __name__ == "__main__":
     app.run()
